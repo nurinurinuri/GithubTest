766eef344bcb41020bd81f7a977c6f422f0c2b9a
DESKTOP-03FF1DA\admin <hojae0604@gmail.com>
2021-04-23 16:35:22 +0900

    수정완료

--- BoardService.java
+++ BoardService.java
@@ -12,13 +12,5 @@ public interface BoardService {
 	//글수정
 	void updateBoard(BoardVO vo);
 	
-	//글 삭제
-	void deleteBoard(BoardVO vo);
-	
-	//글 상세 조회
-	BoardVO getBoard(BoardVO vo);
-	
-	//글 목록 조회
-	List<BoardVO> getBoardList(BoardVO vo);
 	
 }
